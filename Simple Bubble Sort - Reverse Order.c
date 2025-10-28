dabc7dfaa2ace09ebf160acdca0ce089865b442b
Diamond Buchanan <113873656+DBu3hanan@users.noreply.github.com>
2025-10-28 11:37:12 -0500

    Add files via upload

--- Simple Bubble Sort - Reverse Order.c
+++ Simple Bubble Sort - Reverse Order.c
@@ -14,7 +14,7 @@ int main()
      
     for (i = 0; i < n - 1; i++){
          for(j = 0; j < n - i - 1; j++){
-              if(array[j] < array[j +1]){
+              if(array[j] > array[j +1]){
                    temp = array[j];
                    array[j] = array[j +1];
                    array[j +1] = temp;
